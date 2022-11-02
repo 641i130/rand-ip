96d4d54b0706dc6697816274d804116951dec0b1
641i130 <galileoray@gmail.com>
2022-11-02 12:23:39 -0500

    can iterate ish

--- src/main.rs
+++ src/main.rs
@@ -21,14 +21,31 @@ fn split_keep<'a>(r: &Regex, text: &'a str) -> Vec<&'a str> {
 fn subnet(subnet_mask: &String) -> Result<Vec<Ipv4Addr>,Box<dyn std::error::Error>> {
     // GENERATE VECTOR OF IP ADDRESSES IN SUBNET MASK!!!
     let seperator = Regex::new(r"([./])").expect("Invalid regex");
-    let mut cidr:Vec<u8> = Vec::new();
+    let mut cidr:Vec<u32> = Vec::new();
     for c in split_keep(&seperator, subnet_mask) {
         if c != "." && c != "/" {
-            cidr.push(c.parse::<u8>().expect("IP address invalid"));
+            cidr.push(c.parse::<u32>().expect("IP address invalid"));
         }
     }
-
+    if cidr.len() != 5 {
+        return Err("Please use the format '1.1.1.1/8' for example.".into())
+    }
     println!("{:?}",cidr);
+
+    let ipnum = (cidr[0] << 24) | (cidr[1] << 16) | (cidr[2] << 8) | (cidr[3]);
+    let mask:u32 = 0xffffffff << (32 - cidr[4]);
+    
+    let ipstart:u32 = ipnum & mask;
+    //let ipstart:Ipv4Addr = Ipv4Addr::from((ipnum & mask).to_be_bytes());
+    //let ipend:Ipv4Addr = Ipv4Addr::from((ipnum | (mask ^ 0xffffffff)).to_be_bytes());
+    let ipend:u32 = ipnum | (mask ^ 0xffffffff);
+
+    for val in 0..ipend-ipstart {
+        println!("{:?}",Ipv4Addr::from(val.to_be_bytes()));
+    }
+
+    println!("Addresses : \n{:?}\n{:?}",ipstart,ipend); 
+
     return Ok(vec![Ipv4Addr::new(127, 0, 0, 1),Ipv4Addr::new(127, 0, 0, 1),Ipv4Addr::new(127, 0, 0, 1)])
 }
 
